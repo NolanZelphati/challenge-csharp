e9c634bb8e482f3589fbfecfe26eb4577307b7e0
Nolan <nolan.zelphati@ynov.com>
2022-10-03 12:12:24 +0200

    HelloWorld

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using CSharpDiscovery.Quest01;
+
+namespace TestCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HelloWorld_Exercice.HelloWorld());
+        }
+    }
+}
